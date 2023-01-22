eba1b3e62c7be2a7264529088294f2ebc07533eb
Mateusz Piechnik <mateuszpiechnik123@gmail.com>
2023-01-22 11:39:07 -0800

    apdejt szachy

--- SZACHY.c
+++ SZACHY.c
@@ -1,20 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-char rows[8] = {'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+char rows[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
-char board[8][8] = {
-    {'w', 's', 'g', 'k', 'q', 'g', 's', 'w'},
-    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-    {'-', '-', '-', '-', '-', '-', '-', '-'},
-    {'-', '-', '-', '-', '-', '-', '-', '-'},
-    {'-', '-', '-', '-', '-', '-', '-', '-'},
-    {'-', '-', '-', '-', '-', '-', '-', '-'},
-    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-    {'W', 'S', 'G', 'Q', 'K', 'G', 'S', 'W'}};
-
-char boardOneD[64] = {
+char board[64] = {
     'w', 's', 'g', 'k', 'q', 'g', 's', 'w',
     'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
     '-', '-', '-', '-', '-', '-', '-', '-',
@@ -24,80 +13,47 @@ char boardOneD[64] = {
     'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
     'W', 'S', 'G', 'Q', 'K', 'G', 'S', 'W'};
 
-void displayOneD()
+void display()
 {
     int counter = 0;
-    printf("\n\n\n");
-    for (int i = 0; i < sizeof(boardOneD); i++)
+    int j = 0;
+    printf("\n\n ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("   %d", i + 1);
+    }
+    printf("\n  ---------------------------------\nA ");
+    for (int i = 0; i < sizeof(board); i++)
     {
 
         if (counter == 8)
         {
-            printf("\n");
+            j++;
+            printf("|\n  ---------------------------------\n%c ", rows[j]);
             counter = 0;
         }
-        printf("%c ", boardOneD[i]);
+        printf("| %c ", board[i]);
         ++counter;
     }
-    printf("\n\n\n");
+    printf("|\n  ---------------------------------\n\n\n");
 }
 
 void chess_game(char player[])
 {
     int ValueStart = (player[0] - 65) * 8 + (player[1] - 48) - 1; // - 48 bo zero
     int ValueEnd = (player[3] - 65) * 8 + (player[4] - 48) - 1;   // - 48 bo zero
-    int figura = boardOneD[ValueStart]; //pobranie figury z pola startowego
-    boardOneD[ValueStart] = 45; //wyzerowanie pola startowego
-    boardOneD[ValueEnd] = figura; //ustawianei na polu pola startowego
-    displayOneD();
-}
-
-void display()
-{
-    int i, j, k;
-
-    printf("\n");
-    printf(" ");
-    // indeks liczbowy
-    for (i = 0; i < 8; i++)
-    {
-        printf("   %d", i + 1);
-    }
-    printf("\n");
-
-    for (k = 0; k < 8; k++)
-    {
-        printf("  ");
-        for (i = 0; i < 33; i++)
-        {
-            printf("-");
-        }
-
-        printf("\n");
-        printf("%c ", rows[k]);
-        for (j = 0; j < 8; j++)
-        {
-            printf("| %c ", board[k][j]);
-        }
-
-        printf("| \n");
-    }
-
-    printf("  ");
-    for (i = 0; i < 33; i++)
-    {
-        printf("-");
-    }
-
-    printf("\n");
+    int figura = board[ValueStart];                           // pobranie figury z pola startowego
+    board[ValueStart] = 45;                                   // wyzerowanie pola startowego 45 to wartosc znaku '-'
+    board[ValueEnd] = figura;       // ustawianie na polu pola startowego
+    system ("clear");                                 
 }
 
 int main()
 {
-    char sourceMove[7], destMove[3];
+    char sourceMove[7];
     while (1)
     {
-        // display();
+        display();
         printf("\nPodaj pole pionka ktorym chcesz wykonac ruch i jego pole docelowe <np. A0 B4>:\n");
         fgets(sourceMove, sizeof(sourceMove), stdin);
         printf("Ruch:");
